d7a119236accd5f5c5912dd4e1384a3c93fc1e74
Tom Michalsen <tom.michalsen@nokia.com>
2018-04-13 15:58:48 +0200

    Overload operators for ClampedValue class

--- src/pelmeni/math/ClampedValue.hpp
+++ src/pelmeni/math/ClampedValue.hpp
@@ -10,13 +10,40 @@ namespace p2d { namespace math {
     class ClampedValue {
     public:
         ClampedValue(const T& val, const T& fmin, const T& fmax) 
-        : value(val), min(fmin), max(fmax) {;}
+        : value(val), min(fmin), max(fmax) {
+            value = clamp<T>(value, min, max);
+        }
 
-        inline T get() const { return clamp<T>(value, min, max); }
+        inline T get() const { return value; }
+
+        ClampedValue operator + (const T& rhs) const;
+        ClampedValue operator - (const T& rhs) const;
+        ClampedValue operator * (const T& rhs) const;
+        ClampedValue operator / (const T& rhs) const;
     private:
         T value;
         const T min;
         const T max;
     }; // class ClampedValue
+
+    template <typename T>
+    ClampedValue<T> ClampedValue<T>::operator + (const T& rhs) const {
+        return ClampedValue<T>(value + rhs, min, max);
+    }
+
+    template <typename T>
+    ClampedValue<T> ClampedValue<T>::operator - (const T& rhs) const {
+        return ClampedValue<T>(value - rhs, min, max);
+    }
+
+    template <typename T>
+    ClampedValue<T> ClampedValue<T>::operator * (const T& rhs) const {
+        return ClampedValue<T>(value * rhs, min, max);
+    }
+
+    template <typename T>
+    ClampedValue<T> ClampedValue<T>::operator / (const T& rhs) const {
+        return ClampedValue<T>(value / rhs, min, max);
+    }
 } // namespace math
 } // namespace p2d
