e8b2d7e7b84fdae867ec45ea37677b8cbd9dfed4
Tom Michalsen <tomvidm@gmail.com>
2018-07-09 21:10:08 +0200

    Cleaning up directory and removing discontinued classes

--- src/pelmeni/graphics/particles/ParticleSystem.cpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "graphics/particles/ParticleSystem.hpp"
-
-namespace p2d { namespace graphics {
-    
-} // namespace graphics
-} // namespace p2d
--- src/pelmeni/math/geometry/AxialCoordinates.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace math { namespace math {
-    using AxialCoordinate = math::Vector2i;
-}
-}
